ea1002d723a0c2f8366966563e113c2e761955b1
Marcelo Fabri <marcelo.fabri@instacart.com>
2019-04-25 23:04:18 -0700

    Add GitHub API example

---
+++ ExploreRxSwift/View Controllers/GitHubReposVC.swift
@@ -0,0 +1,88 @@
+import UIKit
+import RxSwift
+import Foundation
+
+final class GitHubReposVC: TopicVC, UITableViewDelegate, UITableViewDataSource {
+    private let disposeBag = DisposeBag()
+    private lazy var tableView = UITableView()
+    private let api = GitHubAPI()
+    private var repos: [RepoWithRelease]?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        [
+            tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ].forEach { $0.isActive = true }
+
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(Cell.self, forCellReuseIdentifier: "Cell")
+
+        startObserving()
+    }
+
+    private func startObserving() {
+        let api = self.api
+        api.repos(forUser: "ReactiveX")
+            .map { repos -> [Repo] in
+                repos.sorted(by: { $0.stargazersCount > $1.stargazersCount })
+            }
+            .filter { $0.count > 5 }
+            .flatMap { [weak self] repos -> Observable<[(Repo, Release?)]> in
+                guard let self = self else { return .just([]) }
+
+                let observables = repos.map { self.releases(forRepo: $0) }
+                return Observable.combineLatest(observables)
+            }
+            .observeOn(MainScheduler.instance)
+            .subscribe(onNext: { [weak self] repos in
+                self?.repos = repos.map { RepoWithRelease(repo: $0.0, release: $0.1) }
+                self?.tableView.reloadData()
+            }, onError: { error in
+                print(error)
+            })
+            .disposed(by: disposeBag)
+    }
+
+    private func releases(forRepo repo: Repo) -> Observable<(Repo, Release?)> {
+        return api.releases(forRepo: repo.fullName)
+            .map { (repo, $0.first) }
+            .startWith((repo, nil))
+            .catchErrorJustReturn((repo, nil))
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return repos?.count ?? 0
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let model = repos![indexPath.row]
+
+        cell.textLabel?.text = model.repo.name + " - " + (model.release?.name ?? "")
+        cell.detailTextLabel?.text = String(model.repo.stargazersCount)
+
+        return cell
+    }
+}
+
+private class Cell: UITableViewCell {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .value1, reuseIdentifier: reuseIdentifier)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private struct RepoWithRelease {
+    let repo: Repo
+    let release: Release?
+}
